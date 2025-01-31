e3feb31b97fec1fd6a33f228d40400e86665a9fd
Olivia 🦄 <olinstru@gmail.com>
2025-01-31 16:09:05 +0100

    feat(header): changed css files structure #1

--- app/components/Searchbar.tsx
+++ app/components/Searchbar.tsx
@@ -1,25 +1,9 @@
 "use client"
 
 import { translations } from "../translations"
-// import { useRouter, useSearchParams } from "next/navigation"
 
 export default function Searchbar() {
-  //   const searchParams = useSearchParams()
-  //   const router = useRouter()
-
-  //   function handleSubmit(event) {
-  //     const textInput = event.target.textInput.value
-  //     const searchType = event.target.searchType.value
-  //     const params = new URLSearchParams(searchParams)
-
-  //     event.preventDefault()
-
-  //     params.set("search", textInput)
-  //     params.set("source", searchType)
-  //     router.push(`?${params.toString()}`)
-
   return (
-    // <form onSubmit={(event) => handleSubmit(event)}>
     <form className="searchbar__form">
       <input
         className="searchbar__input"
