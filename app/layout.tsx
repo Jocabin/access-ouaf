edc48a73cc41987fc9dc3fb7fccedce70a5e6086
Olivia 🦄 <olinstru@gmail.com>
2025-03-14 17:59:21 +0100

    cleaned page/layout structures

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import Script from "next/script"
+import Header from "./components/Header"
+import Searchbar from "./components/Searchbar"
+import HeaderMenu from "./components/HeaderMenu"
+import Footer from "./components/Footer"
+import { PrimeReactProvider } from "primereact/api"
+import "primereact/resources/themes/saga-orange/theme.css"
+import "primeicons/primeicons.css"
 
 export const metadata: Metadata = {
   title: "Access-Ouaf",
@@ -18,7 +25,17 @@ export default function RootLayout({
         src="https://kit.fontawesome.com/123bd410f9.js"
         crossOrigin="anonymous"
       />
-      <body>{children}</body>
+      <body>
+        <PrimeReactProvider>
+          <div className="main--container">
+            <Header />
+            <Searchbar />
+            <HeaderMenu />
+            <div className="main--content">{children}</div>
+            <Footer />
+          </div>
+        </PrimeReactProvider>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,8 @@
-import { PrimeReactProvider } from 'primereact/api'
-import 'primereact/resources/themes/saga-orange/theme.css'
-import 'primeicons/primeicons.css'
-import { getAllProducts } from "@/supabase"
-import Header from "./components/Header"
-import Searchbar from "./components/Searchbar"
-import HeaderMenu from "./components/HeaderMenu"
-import Footer from "./components/Footer"
-
 export default async function Home() {
-  console.log(await getAllProducts())
-
   return (
     <>
-      <PrimeReactProvider>
-        <div className="main--container">
-          <Header />
-          <Searchbar />
-          <HeaderMenu />
-          <div className="main--content"></div>
-          <Footer />
-        </div>
-      </PrimeReactProvider>
+      <div className="main--content">
+      </div>
     </>
   )
 }
