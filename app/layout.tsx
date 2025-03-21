adb1dae851be8afa629f5834f53ae77e1c2159ae
Olivia 🦄 <olinstru@gmail.com>
2025-03-21 16:54:13 +0100

    fix/added-missing-font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import Script from "next/script"
+import { Urbanist, Quicksand } from "next/font/google"
+
+const urbanist = Urbanist({ weight: "600", subsets: ["latin"] })
+const quicksand = Quicksand({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Access-Ouaf",
@@ -18,7 +22,15 @@ export default function RootLayout({
         src="https://kit.fontawesome.com/123bd410f9.js"
         crossOrigin="anonymous"
       />
-      <body>{children}</body>
+      <body
+        className={`${quicksand.className} ${urbanist.className}`}
+        style={{
+          "--font-quicksand": quicksand.style.fontFamily,
+          "--font-urbanist": urbanist.style.fontFamily,
+        } as React.CSSProperties}
+      >
+        {children}
+      </body>
     </html>
   )
 }
