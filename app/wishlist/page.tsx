c1fa130d2f5634ccb82dc00ea0c5f33c8dead345
Jocabin <kobyfar@hotmail.com>
2025-03-29 13:43:45 +0100

    add wishlist page

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,7 +1,17 @@
-import { Product, supabase } from "@/supabase";
+import { Product } from "@/types";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import Card from "@/app/components/Card";
 
 export default async function WishlistPage() {
-  const auth_id = "";
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) redirect("/");
+
+  const auth_id = user.id;
   const { data, error } = await supabase
     .from("wishlist")
     .select(
@@ -11,15 +21,27 @@ export default async function WishlistPage() {
         `
     )
     .eq("user_uid", auth_id);
-  console.log(data);
 
   if (!data) return <p>error fetching wishlist: {JSON.stringify(error)}</p>;
 
-  const wishlist: Product[] = data;
+  const wishlist: Product[] = data.map((el) => el.products);
 
   return (
-    <ul>
-      {wishlist && wishlist.map((item) => <li key={item.id}>{item.name}</li>)}
-    </ul>
+    <div className="grid-content">
+      <h2 className="product-grid-title">Wishlist</h2>
+      <div className="products-grid-home">
+        {wishlist.map((product) => (
+          <Card
+            href={"/items/" + product.slug}
+            key={product.id}
+            imageUrl={`${process.env.NEXT_PUBLIC_SUPABASE_URL}${process.env.NEXT_PUBLIC_IMG_URL}${product.img}`}
+            title={product.name}
+            price={`${product.price} €`}
+            width={240}
+            height={352}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
