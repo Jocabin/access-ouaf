2930054dadc9cfe6dc75bd23b418acfa3ccea836
Clément Tutin <clement.tutin.pro@gmail.com>
2025-05-06 12:36:16 +0200

    🎨 Favicons + width = 100vw deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,25 +29,42 @@ export default async function RootLayout({
         crossOrigin="anonymous"
       />
       <body
-        className={`${quicksand.className} ${urbanist.className}`}
-        style={
-          {
-            "--font-quicksand": quicksand.style.fontFamily,
-            "--font-urbanist": urbanist.style.fontFamily,
-          } as React.CSSProperties
-        }
+          className={`${quicksand.className} ${urbanist.className}`}
+          style={
+              {
+                  "--font-quicksand": quicksand.style.fontFamily,
+                  "--font-urbanist": urbanist.style.fontFamily,
+              } as React.CSSProperties
+          }
       >
-        <PrimeReactProvider>
+      <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png"/>
+      <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png"/>
+      <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png"/>
+      <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png"/>
+      <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png"/>
+      <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png"/>
+      <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png"/>
+      <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png"/>
+      <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-icon-192x192.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+      <link rel="manifest" href="/favicons/manifest.json"/>
+      <meta name="msapplication-TileColor" content="#ffffff"/>
+      <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png"/>
+      <meta name="theme-color" content="#ffffff"/>
+      <PrimeReactProvider>
           <div className="main--container">
-            <Header />
-            <Suspense>
-              <Searchbar />
-            </Suspense>
-            <HeaderMenu />
-            <div className="main--content">{children}</div>
-            <Footer />
+              <Header/>
+              <Suspense>
+                  <Searchbar/>
+              </Suspense>
+              <HeaderMenu/>
+              <div className="main--content">{children}</div>
+              <Footer/>
           </div>
-        </PrimeReactProvider>
+      </PrimeReactProvider>
       </body>
     </html>
   );
