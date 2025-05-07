20a7d58e00ccf52a60aaae31a91c25f771421269
Clément Tutin <clement.tutin.pro@gmail.com>
2025-05-07 13:38:34 +0200

    🎨 Use services for user & products fetch

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,22 +1,18 @@
-import { createClient } from '@/utils/supabase/server'
-import {Avatar} from 'primereact/avatar'
+import { fetchUser } from '@/services/users.service'
+import { getProductsByUser} from '@/services/products.service'
 import { formatDistanceToNow } from 'date-fns'
 import { fr } from 'date-fns/locale'
+import { Avatar } from 'primereact/avatar'
+import { Card } from 'primereact/card'
 
 export default async function ProductPage({params}: {
     params: Promise<{ id: string }>
 }) {
-    const supabase = await createClient()
     const { id } = await params
-    const { data: user, errorUser } = await supabase
-        .rpc('get_user_by_id', { uid: id })
+    const user = await fetchUser(id)
+    const annonces = await getProductsByUser(id)
 
-    if (errorUser) {
-        console.error(errorUser)
-        return <div>Erreur lors du chargement des données</div>
-    }
-
-    const memberSince = formatDistanceToNow(new Date(user.created_at), { locale: fr })
+    const memberSince = formatDistanceToNow(new Date(user?.created_at), { locale: fr })
 
     return (
         <div className='flex flex-col items-center gap-10 mt-10'>
@@ -27,6 +23,22 @@ export default async function ProductPage({params}: {
             </div>
             <div>
                 <span>Annonces en ligne</span>
+                <div className="mt-4">
+                    {annonces.length > 0 ? (
+                        <ul className="list-disc list-inside">
+                            {annonces.map((annonce: any) => (
+                                <li key={annonce.id}>
+                                    <Card>
+                                        <span>{annonce.brand}</span>
+                                        <span>{annonce.slug}</span>
+                                    </Card>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>Aucune annonce</p>
+                    )}
+                </div>
             </div>
         </div>
     )
