ad87b109c65fb4cae6179fb5da8aefae3c181b5d
Jocabin <kobyfar@hotmail.com>
2025-05-23 21:13:56 +0200

    Finish payment success page

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,3 +1,17 @@
 export default function PaymentSuccessPage() {
-  return <p>bien ouej</p>;
+  return (
+    <>
+      <h1>Merci pour votre commande</h1>
+
+      <div className="mt-4 mb-6">
+        <p className="leading-6">
+          Votre paiement a été traité avec succès. Nous vous remercions pour
+          votre confiance.
+          <br />
+          L&apos;équipe de Marketplace Animaux vous souhaite une excellente
+          journée.
+        </p>
+      </div>
+    </>
+  );
 }
